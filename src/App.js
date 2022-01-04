235d4049dccb1174b48789a10e5428531bfd9e78
HughesSeanKyle <khughessean@yahoo.com>
2022-01-04 10:55:10 +0200

    Route system

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 import './App.css';
 import {
 	Watchlist,
@@ -8,17 +11,44 @@ import {
 	Navbar,
 } from './components';
 
-function App() {
-	return (
-		<div className="App">
+/*
+  <Route path='/welcome' element={<Home/>} />
+*/
+
+const App = () => (
+	<div className="app">
+		<div className="navbar">
 			<Navbar />
-			<Home />
-			<Cryptocurrencies />
-			<CryptoDetails />
-			<Watchlist />
-			<News />
 		</div>
-	);
-}
+		<div className="main">
+			<Layout>
+				<div className="routes">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/watchlist" element={<Watchlist />} />
+						<Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+						<Route path="/crypto/:coinId" element={<CryptoDetails />} />
+						<Route path="/news" element={<News />} />
+					</Routes>
+				</div>
+			</Layout>
+			<div className="footer">
+				<Typography.Title
+					level={5}
+					style={{ color: 'white', textAlign: 'center' }}
+				>
+					Copyright © 2022
+					<Link to="/"> CryptoSight Inc.</Link> <br />
+					All Rights Reserved.
+				</Typography.Title>
+				<Space>
+					<Link to="/">Home</Link>
+					<Link to="/exchanges">Watchlist</Link>
+					<Link to="/news">News</Link>
+				</Space>
+			</div>
+		</div>
+	</div>
+);
 
 export default App;
